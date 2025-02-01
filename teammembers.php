e14ae9c51ee67cd709c04518a2f33d6705efa802
Sim Shad <samsudeenashad@icloud.com>
2025-02-01 13:51:44 +0530

    update

--- teammembers.php
+++ teammembers.php
@@ -244,13 +244,14 @@
         <div class="row mx-0 pt-5 px-sm-3 px-lg-5 mt-4">
             <div class="col-lg-3 col-md-6 mb-5">
                 <h4 class="text-white text-uppercase mb-4" style="letter-spacing: 3px;">Get In Touch</h4>
-                <p><i class="fa fa-map-marker-alt mr-2"></i>123 Street, New York, USA</p>
+                <p><i class="fa fa-map-marker-alt mr-2"></i>NSBM Green Univercity</p>
                 <p><i class="fa fa-phone-alt mr-2"></i>+94 71 279 0070</p>
                 <p class="m-0"><i class="fa fa-envelope mr-2"></i>atmjayalath@students.nsbm.ac.lk</p>
             </div>
             <div class="col-lg-3 col-md-6 mb-5">
                 <h4 class="text-white text-uppercase mb-4" style="letter-spacing: 3px;">Follow Us</h4>
-                <p>Amet elitr vero magna sed ipsum sit kasd sea elitr lorem rebum</p>
+                <p>Stay connected and never miss an update!<br>
+                Join us on social media and be part of our growing community.</p>
                 <div class="d-flex justify-content-start">
                     <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i class="fab fa-twitter"></i></a>
                     <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
@@ -268,21 +269,21 @@
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 mb-5">
-                <h4 class="text-white text-uppercase mb-4" style="letter-spacing: 3px;">Newsletter</h4>
-                <p>Amet elitr vero magna sed ipsum sit kasd sea elitr lorem rebum</p>
+                <h4 class="text-white text-uppercase mb-4" style="letter-spacing: 3px;">Share Your Ideas</h4>
+                <p>Send us your thoughts, suggestions, or feedback and be part of our journey.</p>
                 <div class="w-100">
                     <div class="input-group">
                         <input type="text" class="form-control border-light" style="padding: 25px;" placeholder="Your Email">
                         <div class="input-group-append">
-                            <button class="btn btn-primary font-weight-bold px-3">Sign Up</button>
+                            <button class="btn btn-primary font-weight-bold px-3">Send Feedback</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="container-fluid text-center text-white border-top mt-4 py-4 px-sm-3 px-md-5" style="border-color: rgba(256, 256, 256, .1) !important;">
-            <p class="mb-2 text-white">Copyright &copy; <a class="font-weight-bold" href="#">Domain</a>. All Rights Reserved.</a></p>
-            <p class="m-0 text-white">Designed by <a class="font-weight-bold" href="https://htmlcodex.com">HTML Codex</a></p>
+            <p class="mb-2 text-white"> &copy; <a class="font-weight-bold" href="#">Web Application Development</a>. Group AD</a></p>
+            <p class="m-0 text-white"> <a class="font-weight-bold" href="https://nsbm.ac.lk">NSBM Green Univercity </a></p>
         </div>
     </div>
     <!-- Footer End -->
